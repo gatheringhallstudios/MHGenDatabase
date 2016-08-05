ac238862a425a42f6ad896791373f967b9aa249d
Joseph Michels <j0mich01+github@gmail.com>
2016-08-05 12:07:37 -0400

    v1.1.1 Release

--- app/src/main/java/com/ghstudios/android/data/database/MonsterHunterDatabaseHelper.java
+++ app/src/main/java/com/ghstudios/android/data/database/MonsterHunterDatabaseHelper.java
@@ -68,7 +68,7 @@ class MonsterHunterDatabaseHelper extends SQLiteAssetHelper {
     //Version 2 - v1.0.1 - Added Alternate Damages/Weaknesses/Ailments
     //Version 3 - v1.0.2 - Fixed issues with some quest data
     //Version 4 - v1.1.0 - Changed Weaknesses / Fixed some data bugs / Default Wishlists+Set
-    //Version 5 - ?????? - Added more localized data / July DLC
+    //Version 5 - v1.1.1 - Added more localized data / July DLC / August DLC
 
     private final Context myContext;
     private SQLiteDatabase myDataBase;
