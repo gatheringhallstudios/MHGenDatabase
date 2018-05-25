afb8c0121af595325db5e72b3ec4e3966ffff798
Carlos Fernandez <cfern1990@gmail.com>
2018-05-25 13:24:57 -0400

    ItemListFragment cleanup and tabs to spaces

--- app/src/main/java/com/ghstudios/android/features/items/ItemListFragment.java
+++ app/src/main/java/com/ghstudios/android/features/items/ItemListFragment.java
@@ -18,6 +18,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.ghstudios.android.MHUtils;
 import com.ghstudios.android.data.classes.Item;
 import com.ghstudios.android.data.database.ItemCursor;
 import com.ghstudios.android.loader.ItemListCursorLoader;
@@ -27,124 +28,99 @@ import com.ghstudios.android.ClickListeners.ItemClickListener;
 import java.io.IOException;
 
 public class ItemListFragment extends ListFragment implements
-		LoaderCallbacks<Cursor> {
+        LoaderCallbacks<Cursor> {
 
-	private ItemListCursorAdapter mAdapter;
-	private String mFilter;
+    private ItemListCursorAdapter mAdapter;
+    private String mFilter;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		// Initialize the loader to load the list of runs
-		getLoaderManager().initLoader(R.id.item_list_fragment, null, this);
+        // Initialize the loader to load the list of runs
+        getLoaderManager().initLoader(R.id.item_list_fragment, null, this);
 
-		mFilter = "";
-	}
+        mFilter = "";
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.fragment_generic_list_search, container,false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_generic_list_search, container,false);
 
-		EditText inputSearch = (EditText) v.findViewById(R.id.input_search);
-		inputSearch.addTextChangedListener(new TextWatcher() {
+        EditText inputSearch = v.findViewById(R.id.input_search);
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+                // When user changed the Text
+                mFilter = cs.toString();
+                getLoaderManager().restartLoader(0, null, ItemListFragment.this);
+            }
 
-		    @Override
-		    public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
-		        // When user changed the Text
-		    	mFilter = cs.toString();
-		    	getLoaderManager().restartLoader(0, null, ItemListFragment.this);
-		    }
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) { }
 
-		    @Override
-		    public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
-		            int arg3) {
-		        // TODO Auto-generated method stub
+            @Override
+            public void afterTextChanged(Editable arg0) { }
+        });
 
-		    }
+        return v;
+    }
 
-		    @Override
-		    public void afterTextChanged(Editable arg0) {
-		        // TODO Auto-generated method stub
-		    }
-		});
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // You only ever load the runs, so assume this is the case
+        return new ItemListCursorLoader(getActivity(), mFilter);
+    }
 
-		return v;
-	}
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        // Create an adapter to point at this cursor
+        mAdapter = new ItemListCursorAdapter(getActivity(), (ItemCursor) cursor);
+        setListAdapter(mAdapter);
+    }
 
-	@Override
-	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-		// You only ever load the runs, so assume this is the case
-		return new ItemListCursorLoader(getActivity(), mFilter);
-	}
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // Stop using the cursor (via the adapter)
+        setListAdapter(null);
+    }
 
-	@Override
-	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-		// Create an adapter to point at this cursor
-		mAdapter = new ItemListCursorAdapter(getActivity(), (ItemCursor) cursor);
-		setListAdapter(mAdapter);
-	}
+    private static class ItemListCursorAdapter extends CursorAdapter {
 
-	@Override
-	public void onLoaderReset(Loader<Cursor> loader) {
-		// Stop using the cursor (via the adapter)
-		setListAdapter(null);
-	}
+        private ItemCursor mItemCursor;
 
-	private static class ItemListCursorAdapter extends CursorAdapter {
+        public ItemListCursorAdapter(Context context, ItemCursor cursor) {
+            super(context, cursor, 0);
+            mItemCursor = cursor;
+        }
 
-		private ItemCursor mItemCursor;
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+            // Use a layout inflater to get a row view
+            LayoutInflater inflater = LayoutInflater.from(context);
+            return inflater.inflate(R.layout.fragment_item_listitem,
+                    parent, false);
+        }
 
-		public ItemListCursorAdapter(Context context, ItemCursor cursor) {
-			super(context, cursor, 0);
-			mItemCursor = cursor;
-		}
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+            // Get the item for the current row
+            Item item = mItemCursor.getItem();
 
-		@Override
-		public View newView(Context context, Cursor cursor, ViewGroup parent) {
-			// Use a layout inflater to get a row view
-			LayoutInflater inflater = (LayoutInflater) context
-					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            TextView itemNameTextView = view.findViewById(R.id.text1);
+            ImageView itemImageView = view.findViewById(R.id.icon);
 
-			return inflater.inflate(R.layout.fragment_item_listitem,
-					parent, false);
-		}
+            String cellText = item.getName();
 
-
-
-		@Override
-		public void bindView(View view, Context context, Cursor cursor) {
-			// Get the item for the current row
-			Item item = mItemCursor.getItem();
-
-			// Set up the text view
-            LinearLayout clickView = (LinearLayout) view.findViewById(R.id.listitem);
-
-			TextView itemNameTextView = (TextView) view
-					.findViewById(R.id.text1);
-			ImageView itemImageView = (ImageView) view
-					.findViewById(R.id.icon);
-
-			String cellText = item.getName();
+            itemNameTextView.setText(cellText);
 
             String cellImage = item.getItemImage();
+            Drawable itemImage = MHUtils.loadAssetDrawable(context, cellImage);
+            itemImageView.setImageDrawable(itemImage);
 
-			itemNameTextView.setText(cellText);
-
-			Drawable itemImage = null;
-
-			try {
-				itemImage = Drawable.createFromStream(
-						context.getAssets().open(cellImage), null);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-			itemImageView.setImageDrawable(itemImage);
-			clickView.setOnClickListener(new ItemClickListener(context, item));
-		}
-	}
+            view.setOnClickListener(new ItemClickListener(context, item));
+        }
+    }
 
 }
