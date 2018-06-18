f9e70d29ae70c485c9d93540d18ed930aa8ecc04
Carlos Fernandez <cfern1990@gmail.com>
2018-06-18 14:34:12 -0400

    Tab to spaces on certain item detail fragments

--- app/src/main/java/com/ghstudios/android/features/items/basicdetail/ItemComponentFragment.java
+++ app/src/main/java/com/ghstudios/android/features/items/basicdetail/ItemComponentFragment.java
@@ -25,117 +25,117 @@ import com.ghstudios.android.mhgendatabase.R;
 import com.ghstudios.android.ClickListeners.ItemClickListener;
 
 public class ItemComponentFragment extends ListFragment implements
-		LoaderCallbacks<Cursor> {
+        LoaderCallbacks<Cursor> {
 
-	private static final String ARG_ITEM_ID = "COMPONENT_ID";
+    private static final String ARG_ITEM_ID = "COMPONENT_ID";
 
-	public static ItemComponentFragment newInstance(long id) {
-		Bundle args = new Bundle();
-		args.putLong(ARG_ITEM_ID, id);
-		ItemComponentFragment f = new ItemComponentFragment();
-		f.setArguments(args);
-		return f;
-	}
+    public static ItemComponentFragment newInstance(long id) {
+        Bundle args = new Bundle();
+        args.putLong(ARG_ITEM_ID, id);
+        ItemComponentFragment f = new ItemComponentFragment();
+        f.setArguments(args);
+        return f;
+    }
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		// Initialize the loader to load the list of runs
-		getLoaderManager().initLoader(R.id.item_component_fragment, getArguments(), this);
-	}
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.fragment_generic_list, null);
-		return v;
-	}
+        // Initialize the loader to load the list of runs
+        getLoaderManager().initLoader(R.id.item_component_fragment, getArguments(), this);
+    }
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_generic_list, null);
+        return v;
+    }
 
 
-	@Override
-	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-		// You only ever load the runs, so assume this is the case
-		long mId = -1;
-		if (args != null) {
-			mId = args.getLong(ARG_ITEM_ID);
-		}
-		return new ComponentListCursorLoader(getActivity(), 
-				ComponentListCursorLoader.FROM_COMPONENT, mId);
-	}
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // You only ever load the runs, so assume this is the case
+        long mId = -1;
+        if (args != null) {
+            mId = args.getLong(ARG_ITEM_ID);
+        }
+        return new ComponentListCursorLoader(getActivity(), 
+                ComponentListCursorLoader.FROM_COMPONENT, mId);
+    }
 
-	@Override
-	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-		// Create an adapter to point at this cursor
-		ComponentListCursorAdapter adapter = new ComponentListCursorAdapter(
-				getActivity(), (ComponentCursor) cursor);
-		setListAdapter(adapter);
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        // Create an adapter to point at this cursor
+        ComponentListCursorAdapter adapter = new ComponentListCursorAdapter(
+                getActivity(), (ComponentCursor) cursor);
+        setListAdapter(adapter);
 
-	}
+    }
 
-	@Override
-	public void onLoaderReset(Loader<Cursor> loader) {
-		// Stop using the cursor (via the adapter)
-		setListAdapter(null);
-	}
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // Stop using the cursor (via the adapter)
+        setListAdapter(null);
+    }
 
-	protected static class ComponentListCursorAdapter extends CursorAdapter {
+    protected static class ComponentListCursorAdapter extends CursorAdapter {
 
-		private ComponentCursor mComponentCursor;
+        private ComponentCursor mComponentCursor;
 
-		public ComponentListCursorAdapter(Context context, ComponentCursor cursor) {
-			super(context, cursor, 0);
-			mComponentCursor = cursor;
-		}
+        public ComponentListCursorAdapter(Context context, ComponentCursor cursor) {
+            super(context, cursor, 0);
+            mComponentCursor = cursor;
+        }
 
-		@Override
-		public View newView(Context context, Cursor cursor, ViewGroup parent) {
-			// Use a layout inflater to get a row view
-			LayoutInflater inflater = (LayoutInflater) context
-					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			return inflater.inflate(R.layout.fragment_component_listitem,
-					parent, false);
-		}
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+            // Use a layout inflater to get a row view
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            return inflater.inflate(R.layout.fragment_component_listitem,
+                    parent, false);
+        }
 
-		@Override
-		public void bindView(View view, Context context, Cursor cursor) {
-			// Get the skill for the current row
-			Component component = mComponentCursor.getComponent();
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+            // Get the skill for the current row
+            Component component = mComponentCursor.getComponent();
 
-			// Set up the text view
-			LinearLayout itemLayout = (LinearLayout) view
-					.findViewById(R.id.listitem);
-			ImageView itemImageView = (ImageView) view.findViewById(R.id.item_image);
-			TextView itemTextView = (TextView) view.findViewById(R.id.item);
-			TextView amtTextView = (TextView) view.findViewById(R.id.amt);
-			TextView typeTextView = (TextView) view.findViewById(R.id.type);
-			
-			Item created = component.getCreated();
-			long createdId = created.getId();
-			
-			String nameText = created.getName();
-			String amtText = "" + component.getQuantity();
-			String typeText = "" + component.getType();
+            // Set up the text view
+            LinearLayout itemLayout = (LinearLayout) view
+                    .findViewById(R.id.listitem);
+            ImageView itemImageView = (ImageView) view.findViewById(R.id.item_image);
+            TextView itemTextView = (TextView) view.findViewById(R.id.item);
+            TextView amtTextView = (TextView) view.findViewById(R.id.amt);
+            TextView typeTextView = (TextView) view.findViewById(R.id.type);
+            
+            Item created = component.getCreated();
+            long createdId = created.getId();
+            
+            String nameText = created.getName();
+            String amtText = "" + component.getQuantity();
+            String typeText = "" + component.getType();
 
-			itemTextView.setText(nameText);
-			amtTextView.setText(amtText);
-			typeTextView.setText(typeText);
-			
-			Drawable i = null;
-			String cellImage = created.getItemImage();
+            itemTextView.setText(nameText);
+            amtTextView.setText(amtText);
+            typeTextView.setText(typeText);
+            
+            Drawable i = null;
+            String cellImage = created.getItemImage();
 
-			try {
-				i = Drawable.createFromStream(
-						context.getAssets().open(cellImage), null);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-			itemImageView.setImageDrawable(i);
+            try {
+                i = Drawable.createFromStream(
+                        context.getAssets().open(cellImage), null);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            itemImageView.setImageDrawable(i);
 
-			itemLayout.setTag(createdId);
-			itemLayout.setOnClickListener(new ItemClickListener(context, created));
-		}
-	}
+            itemLayout.setTag(createdId);
+            itemLayout.setOnClickListener(new ItemClickListener(context, created));
+        }
+    }
 
 }
