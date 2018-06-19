c38124ec551eb262a8283e94c1c7426ff485ac55
Carlos Fernandez <cfern1990@gmail.com>
2018-06-18 23:46:51 -0400

    RecyclerViewFragment auto adds a divider

--- app/src/main/java/com/ghstudios/android/RecyclerViewFragment.kt
+++ app/src/main/java/com/ghstudios/android/RecyclerViewFragment.kt
@@ -2,6 +2,9 @@ package com.ghstudios.android
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +14,7 @@ import com.ghstudios.android.mhgendatabase.R
 /**
  * Creates a fragment that contains a recyclerview.
  * This handles most of the setup and handles a potential memory leak case.
+ * Items are split by a divider
  */
 open class RecyclerViewFragment : Fragment() {
     lateinit var recyclerView: RecyclerView
@@ -26,6 +30,16 @@ open class RecyclerViewFragment : Fragment() {
                 R.layout.fragment_recyclerview_generic,
                 parent,
                 false) as RecyclerView
+
+        // Adds a divider
+        val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+        val divider = DividerItemDecoration(context, layoutManager.orientation)
+        val dividerDrawable = ContextCompat.getDrawable(context!!, R.drawable.generic_divider)
+        dividerDrawable?.let {
+            divider.setDrawable(it)
+        }
+        this.recyclerView.addItemDecoration(divider)
+
         return recyclerView
     }
 
