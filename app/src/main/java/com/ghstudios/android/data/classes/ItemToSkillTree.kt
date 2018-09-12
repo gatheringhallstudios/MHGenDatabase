61e0b678c6a0199a947a3c216c60c8bce3d2c962
Carlos Fernandez <cfern1990@gmail.com>
2018-09-12 19:10:21 -0400

    SkillTreePoints is now immutable. Fixed ASB updates on add/remove decoration.

--- app/src/main/java/com/ghstudios/android/data/classes/ItemToSkillTree.kt
+++ app/src/main/java/com/ghstudios/android/data/classes/ItemToSkillTree.kt
@@ -3,7 +3,7 @@ package com.ghstudios.android.data.classes
 /**
  * Class for ItemToSkillTree. Represents an item associated with a skill tree and a points value
  */
-class ItemToSkillTree(skillTree: SkillTree): SkillTreePoints(skillTree) {
+class ItemToSkillTree(skillTree: SkillTree, points: Int): SkillTreePoints(skillTree, points) {
 
     /**
      * Id of the ItemToSkillTree entry. This is NOT the skilltree id
