19abbbcdf1a3a34ea36671b22781a2b21b80c110
Carlos Fernandez <cfern1990@gmail.com>
2018-09-26 15:39:38 -0400

    Fix ASB part selection mapping, ASB armor selection activity now shows the part in the title.

--- app/src/main/java/com/ghstudios/android/features/armorsetbuilder/armorselect/ArmorSelectActivity.kt
+++ app/src/main/java/com/ghstudios/android/features/armorsetbuilder/armorselect/ArmorSelectActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import com.ghstudios.android.GenericActivity
 import com.ghstudios.android.MenuSection
+import com.ghstudios.android.data.classes.ArmorSet
 import com.ghstudios.android.features.armorsetbuilder.detail.ASBPagerActivity
 import com.ghstudios.android.mhgendatabase.R
 
@@ -16,11 +17,18 @@ class ArmorSelectActivity : GenericActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setTitle(R.string.activity_asb_armor_picker)
-
         val asbPieceIndex = intent.getIntExtra(ASBPagerActivity.EXTRA_PIECE_INDEX, -1)
         val rank = intent.getIntExtra(ASBPagerActivity.EXTRA_SET_RANK, -1)
         val hunterType = intent.getIntExtra(ASBPagerActivity.EXTRA_SET_HUNTER_TYPE, -1)
+
+        setTitle(when (asbPieceIndex) {
+            ArmorSet.HEAD -> R.string.asb_title_select_head
+            ArmorSet.BODY -> R.string.asb_title_select_body
+            ArmorSet.ARMS -> R.string.asb_title_select_arms
+            ArmorSet.WAIST -> R.string.asb_title_select_waist
+            ArmorSet.LEGS -> R.string.asb_title_select_legs
+            else -> R.string.asb_title_select_armor
+        })
 
         viewModel.initialize(asbPieceIndex, hunterType)
     }
