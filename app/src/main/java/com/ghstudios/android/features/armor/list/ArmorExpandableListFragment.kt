9631e085b641fb649a9b034af413f220e8b8cac9
Carlos Fernandez <cfern1990@gmail.com>
2018-10-09 14:36:23 -0400

    ArmorFamilies are now loaded asynchronously (using a viewmodel). Attempt at improved performance.

--- app/src/main/java/com/ghstudios/android/features/armor/list/ArmorExpandableListFragment.kt
+++ app/src/main/java/com/ghstudios/android/features/armor/list/ArmorExpandableListFragment.kt
@@ -1,5 +1,9 @@
 package com.ghstudios.android.features.armor.list
 
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -9,10 +13,37 @@ import android.widget.*
 import android.view.*
 
 import com.ghstudios.android.data.DataManager
+import com.ghstudios.android.data.classes.Armor
 import com.ghstudios.android.mhgendatabase.R
 
-import com.ghstudios.android.data.classes.Armor.Companion.ARMOR_TYPE_BLADEMASTER
+import com.ghstudios.android.data.classes.ArmorFamily
+import com.ghstudios.android.util.loggedThread
 
+/**
+ * ViewModel used to background load armor families and store them.
+ */
+class ArmorFamilyListViewModel: ViewModel() {
+    val dataManager = DataManager.get()
+    val armorFamilyListData = MutableLiveData<List<ArmorFamily>>()
+
+    // loaded family type to prevent double-loading
+    private var hunterType: Int? = null
+
+    fun initialize(hunterType: Int) {
+        if (hunterType == this.hunterType) {
+            return
+        }
+
+        this.hunterType = hunterType
+        loggedThread("ArmorFamily List Loading") {
+            armorFamilyListData.postValue(dataManager.queryArmorFamilies(hunterType))
+        }
+    }
+}
+
+/**
+ * Fragment used to display the list of armor families.
+ */
 class ArmorExpandableListFragment : Fragment() {
     companion object {
         private val ARG_TYPE = "ARMOR_TYPE"
@@ -30,14 +61,17 @@ class ArmorExpandableListFragment : Fragment() {
         }
     }
 
-    private var hunterType: Int = 0
+    private val viewModel by lazy {
+        ViewModelProviders.of(this).get(ArmorFamilyListViewModel::class.java)
+    }
 
     private var adapter: ArmorFamilyListAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        hunterType = arguments!!.getInt(ARG_TYPE, ARMOR_TYPE_BLADEMASTER)
+        val hunterType = arguments?.getInt(ARG_TYPE, Armor.ARMOR_TYPE_BOTH) ?: Armor.ARMOR_TYPE_BOTH
+        viewModel.initialize(hunterType)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -47,17 +81,19 @@ class ArmorExpandableListFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        // todo: use a viewmodel. This was refactored but not thoroughly.
-        val families = DataManager.get().queryArmorFamilies(hunterType)
-        val groups = families
-                .groupBy { it.rarity }
-                .map { ArmorFamilyGroup(it.key, it.value) }
-                .sortedBy { it.rarity }
+        viewModel.armorFamilyListData.observe(this, Observer { families ->
+            if (families == null) return@Observer
 
-        adapter = ArmorFamilyListAdapter(groups)
+            val groups = families
+                    .groupBy { it.rarity }
+                    .map { ArmorFamilyGroup(it.key, it.value) }
+                    .sortedBy { it.rarity }
 
-        val elv = view.findViewById<ExpandableListView>(R.id.expandableListView)
-        elv.setAdapter(adapter)
+            adapter = ArmorFamilyListAdapter(groups)
+
+            val elv = view.findViewById<ExpandableListView>(R.id.expandableListView)
+            elv.setAdapter(adapter)
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
