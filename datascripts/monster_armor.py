3436c47e0c5fb46811382558fd67151e5e4e08ee
Carlos Fernandez <cfern1990@gmail.com>
2019-04-13 03:26:17 -0400

    Added more complex association conditions. Majority rule for most entries.

--- datascripts/monster_armor.py
+++ datascripts/monster_armor.py
@@ -24,8 +24,28 @@ class MonsterDataCollection:
     def by_id(self, id):
         return self.monster_by_id[id]
 
+class MonsterChain:
+    "A chain of monsters, named after the first one. Create using monster DB objects"
+    def __init__(self, entries):
+        self.name = entries[0].name
+        self.entries = entries
+
+        self.names = [entry.name for entry in self.entries]
+        self.ids = [entry._id for entry in self.entries]
+
+    def contains_ids(self, other_ids: set):
+        return other_ids.issubset(set(self.ids))
+
+    def by_id(self, other_ids):
+        "Returns multiple monster objects by id"
+        results = []
+        for entry in self.entries:
+            if entry._id in other_ids:
+                results.append(entry)
+        return results
+
 def create_monster_armor_csv(session: sqlalchemy.orm.Session):
-    # We don't have an armor -> monster mapping file. Try to run a few possible heuristics
+    "Create an armor -> monster mapping file using several heuristics."
     all_armor = (session.query(db.Item)
         .filter(db.Item.type == 'Armor')
         .options(selectinload(db.Item.components))
@@ -35,18 +55,24 @@ def create_monster_armor_csv(session: sqlalchemy.orm.Session):
     monster_data = MonsterDataCollection(session)
 
     # Create monster chains. These list subspecies.
-    # First we hardcode them with names and then turn them to ids
+    # First we hardcode them with names and then turn them to MonsterChain objects
     monster_chains = [
         ["Deviljho", "Savage Deviljho"],
         ["Gore Magala", "Chaotic Gore Magala"],
-        ["Rathian", "Gold Rathian"],
-        ["Rathalos", "Silver Rathalos"],
+        ["Rathian", "Gold Rathian", "Dreadqueen Rathian"],
+        ["Rathalos", "Silver Rathalos", "Dreadking Rathalos"],
         ["Rajang", "Furious Rajang"],
         ["Brachydios", "Raging Brachydios"]
     ]
     monster_chains = [
-        [monster_data.by_name(name)._id for name in chain] for chain in monster_chains
+        MonsterChain([monster_data.by_name(name) for name in chain]) for chain in monster_chains
     ]
+
+    # Mapping for monster -> participating chain idx. Used for some lookups
+    monster_to_chain = {}
+    for chain in monster_chains:
+        for monster_id in chain.ids:
+            monster_to_chain[monster_id] = chain
     
     # Monster Items
     deviant_monster_starter_to_name = {}
@@ -71,8 +97,8 @@ def create_monster_armor_csv(session: sqlalchemy.orm.Session):
         
         # If this item exclusively by a chain of monsters, pick the first one
         for chain in monster_chains:
-            if monster_ids.issubset(set(chain)):
-                item_to_monster_ids[item_id] = next(filter(lambda m: m in monster_ids, chain))
+            if chain.contains_ids(monster_ids):
+                item_to_monster_ids[item_id] = chain.by_id(monster_ids)[0]._id
 
     def create_result(armor, monster_name=None, components=[]):
         return {
@@ -96,6 +122,7 @@ def create_monster_armor_csv(session: sqlalchemy.orm.Session):
 
         key_items = list(filter(lambda c: c.key, components))
         key_item = key_items[0].component_item if key_items else None
+        key_item_id = key_item._id if key_item else None
 
         # Handle Deviant Armor
         if armor.rarity == 11:
@@ -118,19 +145,64 @@ def create_monster_armor_csv(session: sqlalchemy.orm.Session):
 
         # Second pass, check the components, see if anything is possibly unanimaous
         item_names = []
-        monster_ids = set()
+        monster_ids = []
         for component in components:
             item_name = component.component_item.name
             monster_id = item_to_monster_ids.get(component.component_item_id)
             if monster_id:
+                monster_ids.append(monster_id)
                 monster_name = monster_data.by_id(monster_id).name
-                monster_ids.add(monster_id)
                 item_name = f'{item_name} [{monster_name}]'
 
             item_names.append(item_name)
 
-        
+        # inner function to handle the acquisition of the result
+        def resolve_monster_from_participant_key(participant):
+            monster_id = participant[1]
+            if participant[0] == 'chain':
+                monster_id = list(monster_to_chain[monster_id].by_id(monster_ids))[-1]._id
+            
+            return monster_data.by_id(monster_id)
 
+        # Try to count how many of each key there is. Chains share a key.
+        # Find what the first and largest participations are.
+        participations = {}
+        for monster_id in monster_ids:
+            key = ('monster', monster_id)
+            if monster_id in monster_to_chain:
+                key = ('chain', monster_to_chain[monster_id].ids[0])
+            participations.setdefault(key, 0)
+            participations[key] += 1
+
+        participations = list(participations.items())
+        participations.sort(key=lambda p: p[1], reverse=True)
+
+        # If there's only one participant, must have 2 items or must be the key
+        if len(participations) == 1:
+            participant, count = participations[0]
+            if count >= 2 or key_item_id in item_to_monster_ids:
+                monster_name = resolve_monster_from_participant_key(participant).name
+                results.append(create_result(armor, monster_name, components=item_names))
+                continue
+        
+        # If there's more than one participant... must be >= 2 items, or if equal take the key item one
+        # Instant failure if the key is another monster
+        if len(participations) > 1 and participations[0][1] >= 2:
+            participant_best, count_best = participations[0]
+            participant_second_best, count_second_best = participations[1]
+
+            if count_best == count_second_best and key_item_id in item_to_monster_ids:
+                monster_id = item_to_monster_ids[key_item_id]
+                monster_name = monster_data.by_id(monster_id).name
+                results.append(create_result(armor, monster_name, components=item_names))
+                continue
+            elif count_best > count_second_best:
+                monster = resolve_monster_from_participant_key(participant_best)
+                if key_item_id not in item_to_monster_ids or item_to_monster_ids[key_item_id] == monster._id:
+                    results.append(create_result(armor, monster.name, components=item_names))
+                    continue
+
+        # If we get here, it failed to resolve
         unresolved.append(create_result(armor, components=item_names))
         
     save_csv(results + unresolved, 'source_data/monster_armor.csv')
